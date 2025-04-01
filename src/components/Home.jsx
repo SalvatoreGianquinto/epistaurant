686b9488de917eaca6303620d56e178abbdeceeb
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-04-01 11:57:55 +0200

    adds carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap"
+import { Carousel } from "react-bootstrap"
+import pastasciutte from "../data/menu.json"
 
 const Home = function () {
   return (
@@ -7,6 +9,19 @@ const Home = function () {
         <Col xs={12} md={8} lg={6} className="text-center">
           <h1>Epistaurant</h1>
           <h2>Le migliori pastasciutte del Web</h2>
+          <Carousel>
+            {pastasciutte.map((pasta) => {
+              return (
+                <Carousel.Item key={pasta.id}>
+                  <img src={pasta.image} className="w-100" alt={pasta.name} />
+                  <Carousel.Caption>
+                    <h3>{pasta.name}</h3>
+                    <p>{pasta.description}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              )
+            })}
+          </Carousel>
         </Col>
       </Row>
     </Container>
